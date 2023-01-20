f17463405af82f395ebe99cda630e2da40832719
willkgraham <willkgraham@protonmail.com>
2023-01-20 12:44:57 -0700

    d

--- src/assignment02/Binary.java
+++ src/assignment02/Binary.java
@@ -7,31 +7,27 @@ public class Binary {
         int n;
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Input the number you want to convert to binary: ");
+        System.out.print("Enter a number: ");
         n = input.nextInt();
 
+        int first = 0;
+        int second = 2;
+        int third = 4;
+        int fourth = 8;
+        int fifth = 16;
+        int sixth = 32;
+        int seventh = 64;
+        int eighth = 128;
 
+        eighth = n/eighth;
+        seventh = (n-(eighth*128))/64;
+        sixth = (n-((eighth*128)+(seventh*64)))/32;
+        fifth = (n-((eighth*128)+(seventh*64)+(sixth*32)))/16;
+        fourth = (n-((eighth*128)+(seventh*64)+(sixth*32)+(fifth*16)))/8;
+        third = (n-((eighth*128)+(seventh*64)+(sixth*32)+(fifth*16)+(fourth*8)))/4;
+        second = (n-((eighth*128)+(seventh*64)+(sixth*32)+(fifth*16)+(fourth*8)+(third*4)))/2;
+        first = (n-((eighth*128)+(seventh*64)+(sixth*32)+(fifth*16)+(fourth*8)+(third*4)+(second*2)))/1;
 
-        int first;
-        int second;
-        int third;
-        int fourth = 0;
-        int fifth = 0;
-        int sixth = 0;
-        int seventh = 0;
-        int eighth = 0;
-
-        first = n%2;
-        second = ((n-first)/2)%2;
-        third = ((n-second)/2)%2;
-        fourth = third%2;
-        fifth = fourth%2;
-        sixth = fifth%2;
-        seventh = sixth%2;
-        eighth = seventh%2;
-
-
-
-        System.out.printf("%d%d%d%d%d%d%d%d", eighth,seventh,sixth,fifth,fourth,third,second,first);
+        System.out.printf("%d%d%d%d%d%d%d%d", eighth, seventh, sixth, fifth, fourth, third, second, first);
     }
 }
