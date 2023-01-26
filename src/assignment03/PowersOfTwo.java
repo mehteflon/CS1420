20e9ec4d9d1ae832ced81dc112d0af88874f5625
willkgraham <willkgraham@protonmail.com>
2023-01-25 21:28:00 -0700

    d

---
+++ src/assignment03/PowersOfTwo.java
@@ -0,0 +1,12 @@
+package assignment03;
+
+public class PowersOfTwo {
+    public static void main(String[] args)
+    {
+
+        for (int i = 0; i < 17; i++)
+        {
+            System.out.println(Math.pow(2, i));
+        }
+    }
+}
