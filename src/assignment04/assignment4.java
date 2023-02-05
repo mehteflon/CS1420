f8b5878e2c9fa38dfcb0b2bcc0926ecd5c182a3f
Will Graham <willgraham@Wills-MacBook-Pro.local>
2023-02-05 12:48:20 -0700

    fard

---
+++ src/assignment04/assignment4.java
@@ -0,0 +1,10 @@
+package assignment04;
+import javax.swing.*;
+
+public class MyDrawingPanel extends JPanel implements Runnable
+{
+    public static void main (String[] args)
+    {
+        MyDrawingPanel mp = new MyDrawingPanel();
+    }
+}
