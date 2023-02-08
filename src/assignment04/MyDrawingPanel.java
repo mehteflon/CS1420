b4de1027ef2b26e47fb59b1faf44ac376e38b174
willkgraham <willkgraham@protonmail.com>
2023-02-07 18:53:48 -0700

    d

--- src/assignment04/MyDrawingPanel.java
+++ src/assignment04/MyDrawingPanel.java
@@ -1,3 +1,9 @@
+/*
+Will Graham u0982574
+February 5 2023
+This assignment was me playing around with graphics methods and I ended up randomly generating triangles and the colors that fill them.
+ */
+
 package assignment04;
 import javax.swing.*;
 import java.awt.*;
@@ -30,16 +36,24 @@ public class MyDrawingPanel extends JPanel implements Runnable // "extends jpane
 
     public void paint (Graphics g)
     {
-        System.out.println("Hey");
-
         g.setColor(Color.BLACK);
-        g.fillRect(0, 0, 800, 800);
+        g.fillRect(0, 0, 800, 800); // Gives panel black background
 
-        for (int x = 0; x < 800; x += 5)
+        int x1, y1, x2, y2, x3, y3;
+        for (int i = 0; i < 15; i++) // i < whatever # of triangles we want to make, in this case 15
         {
-            g.setColor(new Color(x/800f, 1.0f - (x/800f), 0f));
-            g.drawLine(x, 0, 400, 400);
+            // Block of code below generates random x and y coordinates that we will use in the x/y coordinate arrays that get passed to the .drawPolygon method
+            x1 = (int) (Math.random() * 800);
+            System.out.print(x1);
+            y1 = (int) (Math.random() * 800);
+            x2 = (int) (Math.random() * 800);
+            y2 = (int) (Math.random() * 800);
+            x3 = (int) (Math.random() * 800);
+            y3 = (int) (Math.random() * 800);
 
+            g.setColor(new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256))); // generates random color within the rgb set (0-255)
+            g.drawPolygon(new int[] {x1, x2, x3}, new int[] {y1, y2, y3}, 3); // Draws the triangles, {x1, x2, x3} is the array of x coordinates for the verticies and vice versa for {y1, y2, y3}
+            g.fillPolygon(new int[] {x1, x2, x3}, new int[] {y1, y2, y3}, 3); // Fills triangles in with color
         }
 
 
---
+++ src/lab05/FilterWords.java
@@ -0,0 +1,29 @@
+package lab05;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FilterWords {
+    public static void main (String[] args)
+    {
+        try (Scanner in = new Scanner(new File("C:\\Users\\willk\\IdeaProjects\\CS1420\\src\\lab05\\words.txt"));
+            PrintWriter out = new PrintWriter("C:\\Users\\willk\\IdeaProjects\\CS1420\\src\\lab05\\fives.txt");)
+        {
+            while (in.hasNext())
+            {
+                String word;
+                word = in.next();
+                System.out.println(word);
+
+                if (word.length() < 6)
+                    out.write(word + "\n");
+
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Could not read the words: " + e.getMessage());
+        }
+    }
+}
